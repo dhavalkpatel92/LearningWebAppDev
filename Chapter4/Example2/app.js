18a102301278fabc217ef584b5958f188f23ec33
nikuldchauhan <dhaval301092@gmail.com>
2015-05-03 22:22:59 -0700

    comment-input and .comments binding

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,6 +1,22 @@
 var app = angular.module('comment_sys', []);
 
 app.controller('comment_sysCtrl', function($scope) {
-  $scope.message = 'Angular is pretty cool.';
+    $scope.comment_input = '';
+  $scope.comments = [
+    'This is the first comment!', 
+    'Heres the second one!',
+    'And this is one more.',
+    'Here is another one!'
+  ];
 
+  $scope.addCommentFromInputBox = function(e) {
+        if($scope.comment_input==null)
+        {
+            alert('cant null');
+        }
+        else
+        {
+            $scope.comments.push($scope.comment_input);
+        }
+  };
 });
