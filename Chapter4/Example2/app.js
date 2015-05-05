0ed8d2f7163eccd543d284e2897b8cb9681ac26b
nikuldchauhan <dhaval301092@gmail.com>
2015-05-04 22:09:19 -0700

    Code indented

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -2,21 +2,18 @@ var app = angular.module('comment_sys', []);
 
 app.controller('comment_sysCtrl', function($scope) {
     $scope.comment_input = '';
-  $scope.comments = [
-    'This is the first comment!', 
-    'Heres the second one!',
-    'And this is one more.',
-    'Here is another one!'
-  ];
+    $scope.comments = [
+        'This is the first comment!',
+        'Heres the second one!',
+        'And this is one more.',
+        'Here is another one!'
+    ];
 
-  $scope.addCommentFromInputBox = function(e) {
-        if($scope.comment_input==null)
-        {
+    $scope.addCommentFromInputBox = function(e) {
+        if ($scope.comment_input == null) {
             alert('cant null');
-        }
-        else
-        {
+        } else {
             $scope.comments.push($scope.comment_input);
         }
-  };
+    };
 });
